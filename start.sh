4174ceb0eba378a6a0b21f6e33cacad71e575ca2
Marcus Rambal Preston <128008102+MarcusRambal@users.noreply.github.com>
2025-02-07 18:26:35 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Variables
+REPO_URL="https://github.com/MarcusRambal/DockerScripts.git"
+APP_DIR="app"
+
+
+if [ ! -d "$APP_DIR" ]; then
+    echo "Clonando el repositorio..."
+    git clone "$REPO_URL" "$APP_DIR"
+else
+    echo "El repositorio ya existe. Actualizando..."
+    cd "$APP_DIR"
+    git pull origin main
+    cd ..
+fi
+
+
+echo "Iniciando los contenedores..."
+docker-compose up --build
